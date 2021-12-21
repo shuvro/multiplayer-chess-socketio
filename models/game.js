6ae95b7526192bc8dfd948dda034e54ebe3c9ac2
shuvro <shuvro.dhrubo@gmail.com>
2021-12-21 13:06:46 +0600

    init commit

--- models/game.js
+++ models/game.js
@@ -1,11 +1,11 @@
 //  Game Schema which contains the user the colors and pgn = moves
 // also contains the result of the game
 
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
     , Schema = mongoose.Schema;
 
-var GameSchema = mongoose.Schema({
-    user: { type: Schema.ObjectId, ref: 'User' },
+const GameSchema = mongoose.Schema({
+    user: {type: Schema.ObjectId, ref: 'User'},
     white: String,
     black: String,
     pgn: String,
